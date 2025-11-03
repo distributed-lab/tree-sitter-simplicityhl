ce6e9c217cf7f77e4fe816252b220ca984f1a34d
Volodymyr Herashchenko <cholkegn@gmail.com>
2025-11-03 15:21:24 +0200

    clean highlight query
    add more descriptors, delete unused symbols, add array_fold symbol

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -5,16 +5,37 @@
 [
   "fn"
   "let"
-  "type"
-  "mod"
   "const"
+  "type"
   "match"
+  "mod"
+] @keyword
+
+; Types
+(unsigned_type) @type.builtin
+(builtin_alias) @type.builtin
+(sum_type) @type.builtin
+(option_type) @type.builtin
+(tuple_type) @type.builtin
+(array_type) @type.builtin
+(list_type) @type.builtin
+(alias_name) @type
+
+[
+  "bool"
   "None"
   "Some"
   "Left"
   "Right"
-  "false"
-  "true"
+] @type
+
+; Modules
+(jet_keyword) @module
+(witness_keyword) @module
+(param_keyword) @module
+
+; Functions
+[
   "unwrap_left"
   "unwrap_right"
   "is_none"
@@ -23,34 +44,36 @@
   "panic!"
   "dbg!"
   "fold"
+  "array_fold"
   "for_while"
   "list!"
-] @keyword
+] @function.builtin
 
-; Types
-(unsigned_type) @type
-(builtin_alias) @type
-(sum_type) @type
-(option_type) @type
-(tuple_type) @type
-(array_type) @type
-(list_type) @type
-(alias_name) @type
-
-; Function names
 (function_name) @function
 
 ; Variables and identifiers
 (identifier) @variable
-(witness_name) @variable.special
-(jet) @function
+(witness_name) @constant
 
 ; Literals
 (bin_literal) @number
 (hex_literal) @number
 (dec_literal) @number
+[
+  "false"
+  "true"
+] @number
+
+(function_params) @variable.parameter
 
 ; Brackets and delimiters
+[
+  ","
+  ";"
+  ":"
+  "="
+] @punctuation.delimiter
+
 [
   "("
   ")"
@@ -60,17 +83,13 @@
   "}"
   "<"
   ">"
-  ","
-  ";"
-  ":"
-  "="
-] @punctuation.delimiter
+] @punctuation.bracket
+
+"::" @punctuation.special
 
 ; Operators (if you want extra highlighting for these)
 [
-  "+"
-  "-"
-  "*"
-  "/"
   "=>"
 ] @operator
+
+
